2a6b8bfb0c68af390a6f63dbcc54b095e5657abd
Sahand Amini <sahandaamini@gmail.com>
2020-11-30 23:24:24 -0600

    Fixed warnings

--- Apartments_df.py
+++ Apartments_df.py
@@ -9,14 +9,21 @@ conn = psycopg2.connect(**params)
 cur = conn.cursor()
 
 
-def create_pandas_table(sql_query, database = conn):
+def create_pandas_table(sql_query, database=conn):
     table = pd.read_sql_query(sql_query, database)
     return table
 
+
 def mainOptions():
-    userChoice = input("Please make an initial choice: \n0. Show all apartment complexes in database \n1. Structured Query \n2. Custom Query \n3. Exit Application\n\n")
+    userChoice = input("""Please make an initial choice:
+    0. Show all apartment complexes in database
+    1. Structured Query
+    2. Custom Query
+    3. Exit Application\n""")
+
     return userChoice
-    
+
+
 def newSearch():
     userChoice = input("\nWould you like to start a new search or exit? Please type either 'continue' or 'exit'\n")
     returnNewChoice = ""
@@ -30,14 +37,13 @@ def newSearch():
     return returnNewChoice
 
 
-
 # Creates data frames
 
 apartment = create_pandas_table('SELECT * FROM public."Apartment"')
 amenities = create_pandas_table('SELECT * FROM public."Amenities"')
 apartmentType = create_pandas_table('SELECT * FROM public."Apartment_Type"')
 rating = create_pandas_table('SELECT * FROM public."Rating"')
-room  = create_pandas_table('SELECT * FROM public."Room"')
+room = create_pandas_table('SELECT * FROM public."Room"')
 roomCost = create_pandas_table('SELECT * FROM public."Room_Cost"')
 roomPlan = create_pandas_table('SELECT * FROM public."Room_Plan"')
 socialMedia = create_pandas_table('SELECT * FROM public."Social_Media"')
@@ -47,10 +53,11 @@ staff = create_pandas_table('SELECT * FROM public."Staff"')
 def showAll():
     print(apartment)
 
+
 def structuredQuery():
     aList = input("Please enter the apartments you would like to query, delimited with spaces\n").split(', ')
-    #halfQuery = apartment.apartment_name.isin(aList)
-    #print(apartment[halfQuery])
+    # halfQuery = apartment.apartment_name.isin(aList)
+    # print(apartment[halfQuery])
     filter_by_apartment = apartment[apartment.apartment_name.isin(aList)]
     print(filter_by_apartment)
 
@@ -59,29 +66,33 @@ def structuredQuery():
     filter_by_count_apartment = filter_by_bed_count[filter_by_bed_count.apartment_name.isin(aList)]
     print(filter_by_count_apartment)
 
-    ### Select statement for all rooms, bedroom counts, costs, and amenities, associated with the apartments entered
-    #apartmentQuery = create_pandas_table('SELECT STATEMENT')  
+    # Select statement for all rooms, bedroom counts, costs, and amenities, associated with the apartments entered
+    # apartmentQuery = create_pandas_table('SELECT STATEMENT')
+
 
 def customQuery():
-    inputString = input("\nYou can enter filters for apartment name (aName), number of bedrooms (bedCount), and maximum cost (maxCost). \nEnter your filters as a list in the previous order and all fields must be accurate and complete or the query will fail.  \nFor instance:  Nexa 3 1200 will return all apartments in Nexa that are under 1,200\n" )
+    inputString = input("""\nYou can enter filters for apartment name (aName), number of bedrooms (bedCount), and maximum cost (maxCost).
+    Enter your filters as a list in the previous order and all fields must be accurate and complete or the query will fail.
+    For instance:  Nexa 3 1200 will return all apartments in Nexa that are under 1,200\n""")
     inputList = inputString.split()
-    queryResult = create_pandas_table('SELECT public."Apartment".apartment_name, bedrooms_cost, public."Room".floor_plan, bedrooms FROM public."Apartment", public."Apartment_Type", public."Room", public."Room_Plan", public."Room_Cost" WHERE public."Apartment".apartment_name = public."Apartment_Type".apartment_name AND public."Apartment".apartment_name = public."Room".apartment_name AND public."Apartment".apartment_name = public."Room_Plan".apartment_name AND public."Apartment".apartment_name = public."Room_Cost".apartment_name AND public."Room".floor_plan = public."Room_Cost".floor_plan AND public."Apartment".apartment_name = ' + "'" + inputList[0] + "'" +' AND public."Room_Plan".bedrooms = '+ inputList[1] + ' AND bedrooms_cost < ' + inputList[2] + ';')
+    queryResult = create_pandas_table('SELECT public."Apartment".apartment_name, bedrooms_cost, public."Room".floor_plan, bedrooms FROM public."Apartment", public."Apartment_Type", public."Room", public."Room_Plan", public."Room_Cost" WHERE public."Apartment".apartment_name = public."Apartment_Type".apartment_name AND public."Apartment".apartment_name = public."Room".apartment_name AND public."Apartment".apartment_name = public."Room_Plan".apartment_name AND public."Apartment".apartment_name = public."Room_Cost".apartment_name AND public."Room".floor_plan = public."Room_Cost".floor_plan AND public."Apartment".apartment_name = ' + "'" + inputList[0] + "'" + ' AND public."Room_Plan".bedrooms = ' + inputList[1] + ' AND bedrooms_cost < ' + inputList[2] + ';')
     print(queryResult)
 
+
 print("\n*********** Welcome to the Tempe Apartment Assist Tool ***********")
+
 menuChoice = mainOptions()
 
 
-
-while(menuChoice != "3" and menuChoice != "exit"):
+while menuChoice != "3" and menuChoice != "exit":
     # valid input
-    if(menuChoice == "0"):
+    if menuChoice == "0":
         showAll()
         menuChoice = newSearch()
-    elif(menuChoice == "1"):
+    elif menuChoice == "1":
         structuredQuery()
         menuChoice = newSearch()
-    elif(menuChoice == "2"):
+    elif menuChoice == "2":
         byAmenities()
         menuChoice = newSearch()
     else:
@@ -91,4 +102,3 @@ cur.close()
 conn.close()
 
 print("Thank you for using the Tempe Apartment Assist Tool!")
-
